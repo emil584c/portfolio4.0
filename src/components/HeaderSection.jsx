c0cb805b2baa036c80fa26637f49948afd7e46f9
emil584c <105641442+emil584c@users.noreply.github.com>
2024-02-06 16:45:23 +0100

    site name change

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -43,7 +43,7 @@ function HeaderSection() {
             to={"/"}
             className="header-section__logo"
           >
-            lind.gg
+            elind.dk
           </Link>
           <ul>
             <li>
